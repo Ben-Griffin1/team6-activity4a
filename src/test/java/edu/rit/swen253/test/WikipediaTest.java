fffc86cc2a9d2da244c024642c2d8b06c68db882
Ben Griffin <bgg6007@rit.edu>
2024-11-07 10:30:00 -0500

    files created wikipedia

---
+++ src/test/java/edu/rit/swen253/test/WikipediaTest.java
@@ -0,0 +1,9 @@
+package edu.rit.swen253.test;
+
+import java.util.logging.Logger;
+
+
+public class WikipediaTest {
+      private static final Logger logger = Logger.getLogger(WikipediaTest.class.getName());
+
+}
